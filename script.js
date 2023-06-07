1081b589afb8da93dc06586c4916b9fa8bca5401
Amrit Singh <amritsingh@innotechteam.in>
2023-06-07 18:53:12 +0530

    working on weather app

--- script.js
+++ script.js
@@ -1,37 +1,110 @@
-const apiKey= "4a29a0775a368ac4bd04e1d32c70f1c8";
-const apiUrl="https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+const apiKey = "4a29a0775a368ac4bd04e1d32c70f1c8";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const card = document.querySelector(".card");
 
-async function checkWeather(city){
-  const response = await fetch (apiUrl + city +`&appid=${apiKey}`);
-  var data = await response.json();
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  const data = await response.json();
   console.log(data);
-  
+
+  if (data.cod === "404" || data.cod === "400") {
+    // Display predefined message with white background
+    
+    document.querySelector(".city").innerHTML = "City not found";
+    document.querySelector(".temp").innerHTML = "";
+    document.querySelector(".humidity").innerHTML = "";
+    document.querySelector(".wind").innerHTML = "";
+    document.querySelector(".weather-icon").style.display = "none";
+    document.querySelector(".details").style.display = "none";
+  } else {
+     // Display weather information
+  document.querySelector(".humidity").style.display = ""; // Reset display property
+  document.querySelector(".wind").style.display = ""; // Reset display property
+  document.querySelector(".card").style.backgroundColor = "";
   document.querySelector(".city").innerHTML = data.name;
   document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
   document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+  const weatherIcon = document.querySelector(".weather-icon");
+  const iconCode = data.weather[0].icon;
+  const iconFilename = getIconFilename(iconCode); // Function to get the icon filename based on the icon code
+  const iconPath = `./icons/${iconFilename}`; // Replace with the actual path to your icons
   
- // Set background image based on time of day
- const currentTime = new Date().getHours();
- console.log(currentTime);
- if (currentTime >= 6 && currentTime < 18) {
-   card.style.backgroundImage = 'url("https://upload.wikimedia.org/wikipedia/commons/c/c5/Seven_Sisters_daytime.jpg")';
-
-  } 
- else if (currentTime >= 18 || currentTime < 6) {
-   card.style.backgroundImage = 'url("https://cdn.pixabay.com/photo/2016/11/25/23/15/moon-1859616_640.jpg")';
- } else {
-   card.style.backgroundImage = 'url("https://upload.wikimedia.org/wikipedia/commons/c/c5/Seven_Sisters_daytime.jpg")';
-   
- }
- card.style.backgroundRepeat = "no-repeat"; // Set background image to not repeat
- card.style.backgroundSize = "cover"; // Scale the background image to cover the entire card
- card.style.backgroundPosition = "center"; // Position the background image in the center
-
+  weatherIcon.src = iconPath;
+  weatherIcon.alt = data.weather[0].description;
+  }
+  function getIconFilename(iconCode) {
+    // Map icon codes to your icon filenames or paths
+    switch (iconCode) {
+      case "01d":
+        return "01d.png";
+        case "01n":
+          return "01n.png";
+      case "02d":
+        return "02d.png";
+        case "02n":
+          return "02n.png";
+      case "03d":
+        return "03d.png";
+        case "03n":
+          return "03n.png";
+      case "04d":
+        return "04d.png";
+        case "04n":
+          return "04n.png";
+      case "09d":
+        return "09d.png";
+        case "09n":
+          return "09n.png";
+      case "10d":
+        return "10d.png";
+        case "010n":
+          return "010n.png";
+      case "11d":
+        return "11d.png";
+        case "11n":
+          return "11n.png";
+      case "13d":
+        return "13d.png";
+        case "13n":
+          return "13n.png";
+      case "50d":
+        return "50d.png";
+        case "50n":
+          return "50n.png";
+      default:
+        return "unknown.png"; // Default icon for unknown weather conditions
+    }
+  }
 }
-searchBtn.addEventListener("click", ()=>{
+searchBtn.addEventListener("click", () => {
   checkWeather(searchBox.value);
-})
+});
+
+// Get user's current location
+function getUserLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      console.log(latitude,"    ",longitude)
+      const locationApiUrl = `https://api.openweathermap.org/data/2.5/weather?&units=metric&lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+      fetch(locationApiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+          const city = data.name;
+          checkWeather(city);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    });
+  } else {
+    console.log("Geolocation is not supported by this browser.");
+  }
+}
+
+getUserLocation();
